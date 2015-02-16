3dcaeaec512d0fefb674ff7240ba7e28fd1977ae
jarpi <ebls123@gmail.com>
2015-02-16 14:33:18 +0100

    - Added tests - Some refactoring ideas

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -4,24 +4,26 @@ function Item(name, sell_in, quality) {
   this.quality = quality;
 }
 
-var items = []
+// var items = []; 
 
-items.push(new Item('+5 Dexterity Vest', 10, 20));
+/* items.push(new Item('+5 Dexterity Vest', 10, 20));
 items.push(new Item('Aged Brie', 2, 0));
 items.push(new Item('Elixir of the Mongoose', 5, 7));
 items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
 items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
-items.push(new Item('Conjured Mana Cake', 3, 6));
+items.push(new Item('Conjured Mana Cake', 3, 6)); */ 
 
-function update_quality() {
+function update_quality(items) { 
+var types = {'backStage':'Backstage passes to a TAFKAL80ETC concert'}; 
+
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+    if (items[i].name != 'Aged Brie' && items[i].name != types.backStage) {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
           items[i].quality = items[i].quality - 1
         }
       }
-    } else {
+    } else { 
       if (items[i].quality < 50) {
         items[i].quality = items[i].quality + 1
         if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
