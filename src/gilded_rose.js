b679ed59a51b6608b5d61f32132e69c2eed3837b
jarpi <ebls123@gmail.com>
2015-02-16 17:33:55 +0100

    Revert 9409854..95503aa
    This rolls back to commit 94098545aeeaf0027ffa2b9000834cc6f2c83cb2.

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -13,68 +13,56 @@ items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
 items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6)); 
 
-var types = {
-normal:'Normal Item',
-backStage:'Backstage passes to a TAFKAL80ETC concert', 
-agedBrie:'Aged Brie', 
-sulfuras:'Sulfuras, Hand of Ragnaros'
-};
-
 function update_quality(items) { 
-    for (var i = 0; i < items.length; i++) {
-      var currentItem = items[i];
-      var currentItemType = getItemType(currentItem.name);
-      if (currentItemType == 'normal' && currentItem.quality > 0){
-        currentItem.quality = currentItem.quality - 1;
+var types = {
+'backStage':'Backstage passes to a TAFKAL80ETC concert', 
+'agedBrie':'Aged Brie', 
+'sulfuras':'Sulfuras, Hand of Ragnaros'
+}; 
+  for (var i = 0; i < items.length; i++) {
+    var currentItem = items[i];
+    if (currentItem.name != types.agedBrie && currentItem.name != types.backStage) {
+      if (currentItem.quality > 0) {
+        if (currentItem.name != types.sulfuras) {
+          currentItem.quality = currentItem.quality - 1
+        }
       } 
-      else 
-        if (currentItem.quality < 50) { 
-      	var toAdd = 1; 
-      	if (currentItemType == types.backStage) {
-      		if (currentItem.sell_in < 11) toAdd+=1; 
-      		if (currentItem.sell_in < 6) toAdd +=1; 
-      	} 
-        if (currentItemType == types.backStage && currentItem.sell_in < 11 && currentItem.quality < 50) { 
-          toAdd+= 1;  
-          if (currentItem.sell_in<6) toAdd+=1; 
-        }  
-        currentItem.quality = currentItem.quality + toAdd; 
+    } else { 
+      if (currentItem.quality < 50) {
+        currentItem.quality = currentItem.quality + 1
+        if (currentItem.name == types.backStage) {
+          if (currentItem.sell_in < 11) {
+            if (currentItem.quality < 50) {
+              currentItem.quality = currentItem.quality + 1
+            }
+          }
+          if (currentItem.sell_in < 6) {
+            if (currentItem.quality < 50) {
+              currentItem.quality = currentItem.quality + 1
+            }
+          }
+        }
       }
-
-      if (currentItem.name != types.sulfuras) {
-        currentItem.sell_in = currentItem.sell_in - 1;
-      }
-
-      if (currentItem.sell_in < 0 && currentItemType == Types.normal && currentItemQuality>0) {
-      	currentItem.quality = currentItem.quality - 1
-        if (currentItemType == types.backStage) {
+    }
+    if (currentItem.name != types.sulfuras) {
+      currentItem.sell_in = currentItem.sell_in - 1;
+    }
+    if (currentItem.sell_in < 0) {
+      if (currentItem.name != types.agedBrie) {
+        if (currentItem.name != types.backStage) {
+          if (currentItem.quality > 0) {
+            if (currentItem.name != types.sulfuras) {
+              currentItem.quality = currentItem.quality - 1
+            }
+          }
+        } else {
           currentItem.quality = currentItem.quality - currentItem.quality
-        } 
-        if (currentItemType == types.agedBrie) {
-          if (currentItem.quality < 50) {
-            currentItem.quality = currentItem.quality + 1
+        }
+      } else {
+        if (currentItem.quality < 50) {
+          currentItem.quality = currentItem.quality + 1
         }
       }
     }
   }
-}
-
-
-function getQuality(item) { 
-
-}
-
-function getItemType(itemName){
-  var type = types.normal;
-  if (itemName==types.backStage){
-    type = types.backStage;
-  }
-  else if(itemName==types.agedBrie){
-    type = types.agedBrie;
-  }
-  else if(itemName==type.sulfuras){
-    type = types.sulfuras;
-  }
-
-  return type;
 }
